158d7b1b8929c2a4970185a9a76b585685aa4df3
ahxc <ahxc1020@163.com>
2020-12-02 19:18:45 +0800

    20.12.2

--- 25-面试题收集/main.js
+++ 25-面试题收集/main.js
@@ -9,7 +9,7 @@
 // 当一个状态改变之后，与这个状态相关的事务也立即随之改变，
 // 包括数据状态改变引起的dom元素的改变或一些方法的启用。
 // 数据模型是普通的JavaScript对象。当修改它们时，视图会进行更新。
-// vue是基于defineproperty来实现数据响应的
+// vue是基于defineProperty来实现数据响应的
 // 定义对象obj的新属性或者修改
 Object.defineProperty(obj, props, value)
 Object.definePropertys(obj, {props:value, props:value...})
@@ -17,7 +17,7 @@ Object.definePropertys(obj, {props:value, props:value...})
 {
 	configurable: true,// 是否可通过delete删除该属性
 	enumerable: true,// 是否可通过foreach, for等等迭代
-	get:,// 重要，todo中用到过，访问该属性时调用的函数，默认undefin
+	get:,// 重要，todo中用到过，访问该属性时调用的函数，默认undefind
 	set:,// 重要，结合get，给属性设置新值调用函数，默认undefined，接收唯一参数
 	// get set在函数中是默认存在的，只不过都是undefined
 }
