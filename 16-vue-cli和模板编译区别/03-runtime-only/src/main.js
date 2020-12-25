ded77ac6d20d3f604fa66899f85c30f5b8b925af
ahxc <ahxc1020@163.com>
2020-12-25 12:43:41 +0800

    ES6 this理解补充

--- 16-vue-cli和模板编译区别/03-runtime-only/src/main.js
+++ 16-vue-cli和模板编译区别/03-runtime-only/src/main.js
@@ -8,6 +8,6 @@ new Vue({
   // template在打包时就编译成render
   render: h => h(App)
   // 方法1.h函数可比作：createdElemnet("标签", {标签的属性：值,}, ["dom"])
-  // 标签指id，属性即class，name等，dom即<html>包裹的元素
+  // 标签指id，属性即class，name等，dom即<html>包裹的元素，即render函数
   // <div id=h2 class="">dom</div>
 })
